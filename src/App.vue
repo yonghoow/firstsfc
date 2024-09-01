d480da56fbf1c300db673de1e1c106d532a8ab05
admin <admin@admins-MacBook-Air.local>
2024-09-01 17:45:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Todo List</h1>
+  <p>In this example, there are three elements on the root of the component so we use v-bind="$attrs" to define which element should receive the fallthrough style attribute that gives breen background-color.</p>
+  <ul>
+    <todo-item 
+      v-for="x in items"
+      :key="x"
+      :item-name="x"
+      style="background-color: lightgreen;"
+    />
+  </ul>
+  <input placeholder="Add things to do here" v-model="newItem" @keydown.enter="addItem">
+  <button @click="addItem">Add</button> 
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newItem: '',
+        items: ['Buy apples', 'Make pizza', 'Mow the lawn']
+      };
+    },
+    methods: {
+      addItem() {
+        this.items.push(this.newItem);
+        this.newItem = '';
+      }
+    }
+  }
+</script>
+
+<style>
+  ul {
+    width: 150px;
+    list-style-type: none;
+    padding-left: 10px;
+  }
+</style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>
+      {{ foodName }}
+      <img src="/img_quality.svg" v-show="isFavorite">
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
+  </div>  
+</template>
+  
+<script>
+  export default {
+    props: ['foodName', 'foodDesc', 'isFavorite'],
+    emits: ['toggle-favorite'],
+    methods: {
+      toggleFavorite() {
+        this.$emit('toggle-Favorite', this.foodName);
+      }
+    }
+  };
+</script>
+  
+<style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
